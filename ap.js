cf68119f56ea19438948b319fdaf62a3acf1586c
Student <student@Admins-iMac.local>
2019-01-22 11:27:48 -0600

    99 uploads

--- ap.js
+++ ap.js
@@ -1,5 +1,20 @@
-let friends= [lou, kent , cam, pete, lee]
-for(var i = 0; i < 100 i++){
-
+let friends = ['lou', 'kent', 'cam', 'pete', 'lee'];
+let addFriend = document.getElementById('addFriend');
+addFriend.addEventListener('click', newFriend)
+function newFriend(){
+    let nameBox = document.getElementById('nameBox').value;
+    if (nameBox != ""){
+        friends.push(nameBox)
+        console.log(friends)
+    }
 }
-console.log(i);
+function FriendList(){ for (var i = 0; i < friends.length; i++) {
+    for (var j = 99; j > 0; j--) {
+        if(j == 1 ){
+        console.log(j + " line of code in the file, " + j + " line of code " + friends[i] + " strikes one out clears it all out, no more lines of code in the file")
+        }else{
+              console.log(j + " lines of code in the file, " + j + " lines of code " + friends[i] + " strikes one out clears it all out, " + (j - 1) + " lines of code in the file")
+        }
+    }
+}
+}
